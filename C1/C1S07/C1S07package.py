bbb95fcb27c4effcea5b5ade05b1e33cce7c0006
lpjlsing <lpjlsing@163.com>
2019-06-05 16:16:43 +0800

    060519-3

--- C1/C1S07/C1S07package.py
+++ C1/C1S07/C1S07package.py
@@ -2,12 +2,11 @@
 # _init.py 文件可以区分文件夹和包，当此文件存在是可认为是一个包
 # 包- 模块- 类- 函数、变量(类的本身特性）
 # import module_name(包括命名空间)，import只能导入模块
-import C1S07range as aa
-print(aa.b)
+import C1S06.C1S06test1 as aac
+print(aac.b)
 
 #from module import a    # 可以直接导入具体的变量、模块
-from C1S06test1 import a
+from C1S06.C1S06test1 import 
 
 from C1S06 import * # *可以导入全部的变量，导入的变量为C1S06中被内置模块所定义的变量
     print(a)
-
