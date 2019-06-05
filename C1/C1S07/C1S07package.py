cf267ed7a514fc53639fbf0ecd7767f01bf9f457
lpjlsing <lpjlsing@163.com>
2019-06-06 01:00:26 +0800

    060619-1

--- C1/C1S07/C1S07package.py
+++ C1/C1S07/C1S07package.py
@@ -6,7 +6,7 @@
 import os,sys   #添加系统的相对路径和绝对路径
 BASE_DIR = os.path.dirname( os.path.dirname( os.path.abspath(__file__)))
 print(BASE_DIR)
-sys.path.append(BASE_DIR) # 添加文件路径到系统变量中
+sys.path.append(BASE_DIR) # 添加文件路径到系统变量中，或者使用 cd C1S06 来定位需要的包
 
 import C1S06.C1S06test1 as aac
 print(aac.b)
@@ -17,3 +17,7 @@ print(aad.c)
 
 # from C1S06 import *    # *可以导入全部的变量，导入的变量为C1S06中被内置模块所定义的变量
 #     print(a)
+
+from C1S06 import (C1S06test1, C1S06test2\
+C1S06test3) # 可以通过 () 来 import多个文件
+print(C1S06test1)
---
+++ C1/C1S07/__init__.py
@@ -0,0 +1,7 @@
+# 导入这个包后，这个文件里的函数自动执行
+# 可以在 __init__文件里配置包的一些初始化操作，比如批量导入、定义内置变量，
+import sys
+import datetime
+import io
+
+print(sys.path)
