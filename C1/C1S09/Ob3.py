d088e1f3758b6dc193ded4805d5c9db310374fad
lpjlsing <lpjlsing@163.com>
2019-06-07 01:54:49 +0800

    class_060719_1

--- C1/C1S09/Ob3.py
+++ C1/C1S09/Ob3.py
@@ -9,7 +9,7 @@ class B():
     name = '' # 类变量，定义在类中，但类变量最好不要定义为特定值
     age = 0
     def __init__(self,name,age): #这个self可以定义为任意变量的标识
-        self.name = name # name这个实例变量最好是定义在对象中，而不是定义在类中
+        self.name = name # name这个实例变量最好是定义在对象中，而不是定义在类中成为类变量
         self.age = age # self声名参数可以用来保存对象的实例变量
 
 B1 = B('b1', 19)
