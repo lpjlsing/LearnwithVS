ea78e95ad7c10d327849908731ac1de382d89564
lpjlsing <lpjlsing@163.com>
2019-06-11 00:10:10 +0800

    grammer_061119

---
+++ C1/C1S11&12Grammers/Anonymous1.py
@@ -0,0 +1,79 @@
+""" 
+闭包
+
+匿名函数: lambda表达式 map reduce filter
+         map, filter在返回列表，需要 list函数输出列表 ******
+
+lambda:
+    lambda parameter_list: expression      :后面必须是表达式
+    算子
+
+三元表达式：条件为真时返回的结果 if 条件 else 条件为假时返回的结果
+
+map: map(function, *iterables)，map的输出通过list()函数导出，可以优化代码，但没有提高代码效率
+
+reduce：处于 functools包中, reduce(function, sequence[, initial]) 
+        连续计算：连续调用函数，最后返回一个数值或字符串    ******
+        必须传入2个元素
+        初始调用列表前两个元素，依次把接下来的列表元素做为reduce所需的参数，只输出一个值
+        第二个列表参数位值可以不出现，此时初始值默认取第一个列表的前连个元素
+        第二个列表参数可以使任意类型列表，字符串，数字等 ******
+大数据中，map/reduce 是一种编程模型，map即是映射，reduce是归约， 主要用于并行计算 ******
+
+filter：可以判断真假1，0  过滤器
+        判断大小写等
+
+
+
+"""
+
+#匿名函数
+def add(x,y):
+    return x + y
+
+f = lambda x,y: x+y  # 匿名函数， ：后面必须是lambda表达式
+print(f(1,2))
+
+x1 = 1
+y1 = 2
+r = x1 if x1 > y1 else y1  # 三元表达式
+print(r)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# map函数
+list_x = [1,2,3,4,5,6]
+list_y = [9,8,7]
+def  s(x):
+    return x * x
+
+r = map(s, list_x) # map(func, *iterables)
+print('map函数: ',list(r)) # list导出map 结果
+
+r2 = map(lambda x,y: x * x, list_x, list_y) # map通过匿名函数表达，可以传入多个list
+print('map通过lambda表示：', list(r2)) # 多个列表同时传入时，list 传出较小的list
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# reduce: 连续计算
+from functools import reduce
+list_x2 = ['1','2','3']
+r3 = reduce(lambda x,y: x * y, list_x, 'a') # reduce 连续调用lambda
+                                            # 第二个列表参数位值可以不出现，此时初始值默认取第一个列表的前连个元素
+                                            # 第二个列表参数可以使任意类型列表，字符串，数字等
+r4 = reduce(lambda x,y: x + y, list_x2, 'a')
+
+print(r3)
+print(len(r3)) # r3有720位
+print(r4)
+print(len(r4))
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# filter:
+list_f = [1,2,3,4,5]
+list_f2 = ['1','a','B','c']
+r5 = filter(lambda x: x, list_f)
+r6 = filter(lambda x: x, list_f2)
+# r6 = filter(lambda x: x, list_x, list_f2)
+
+print(r5)
+print(list(r5))
+print(list(r6))
---
+++ C1/C1S11&12Grammers/Decorator1.py
@@ -0,0 +1,94 @@
+""" 
+对修改封闭，对扩展开放
+
+import time
+
+装饰器：  代码复用，代码封闭安全性
+    不改变原有函数，增加新功能  ******
+    装饰器外部函数，装饰， 跟闭包相比没有环境变量。内部函数用于封装
+    @：语法堂之一通过 @装饰器， 保持原来的调用方式不改变  ******
+    @ 是AOP，python设计模式  ******
+    当装饰器跟特定函数绑定时，装饰器没有意义  ******
+    def wrapper2(*args,**kw): -->可以传入任意类型参数，比如 通用参数、关键字参数  ******
+    在所需要函数上加入装饰器，可以通过flask是函数成为一个控制器 *****
+
+ 带参数装饰器： 利用可变参数，如*args
+
+时间戳
+
+flask：控制器
+
+"""
+
+import time
+
+# 非装饰器：定义函数没有体现出与新函数的关联
+def t1():
+ #   print(time.time())
+    print('Time is: ')
+t1()   # 时间戳
+
+def t2():
+#    print(time.time())
+    print('Time is: ')
+
+def t(func): # 函数作为以一个函数的参数传入
+    print(time.time())
+    func()
+
+t(t1)
+t(t2)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+#装饰器：
+
+def decorator(func): # 装饰器外部函数，装饰， 跟闭包相比没有环境变量
+    def wrapper(): # 内部函数，封装
+        print(time.time())
+        func()  # 这里把func()参数函数封装进内部函数，func作为外部函数参数
+    return wrapper
+
+def t3():
+    print('Time is: ')
+
+@decorator  # 通过@装饰器， 保持原来的调用方式不改变
+            # 装饰器真正意义
+def t4():
+    print('Time is: ')
+
+print('这个装饰器需要通过变量来调用：')
+dd = decorator(t3)
+dd()  # 这个装饰器需要通过变量来调用  ******
+
+print('装饰器通过@语法堂不改变调用方式：')
+t4()  # 这个装饰器需要通过变量来调用  ******
+
+# 带参数装饰器： 利用可变参数
+print('带参数装饰器：')
+def decorator2(func): # 装饰器外部函数，装饰， 跟闭包相比没有环境变量
+    def wrapper2(*args,**kw): #  *args 表示通用参数，这个可以任意定义
+                              #  **kw 传入关键字参数
+        print(time.time())
+        func(*args, **kw)  # 这里把func()参数函数封装进内部函数，func作为外部函数参数
+    return wrapper2
+
+def t5(func_name):
+    print('Time is: ' + func_name)
+
+@decorator2
+def t6(func_name1,func_name2):
+    print('Time is: ' + func_name1)
+    print('Time is: ' + func_name2)
+
+t5('test func')
+t6('test func1','test func2')
+
+@decorator2
+def t7(func_name3,func_name2, **kw):   # 装饰器带关键字参数
+                                       #
+    print('Time is: ' + func_name3)
+    print('Time is: ' + func_name2)
+    print(kw) # 打印kw关键字参数
+
+t7('test **kw func3','test **kw func4', a = 1, b = 2, c = 'd')
+
