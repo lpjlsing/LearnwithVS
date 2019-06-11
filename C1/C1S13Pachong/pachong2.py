9904c4801995bcdb04b5828297b47b0b57d3e375
lpjlsing <lpjlsing@163.com>
2019-06-12 01:12:53 +0800

    pachong_sort_061219

--- C1/C1S13Pachong/pachong2.py
+++ C1/C1S13Pachong/pachong2.py
@@ -3,7 +3,14 @@
 
 加入定时器
 
-精炼爬虫模块, stripe()函数
+精炼爬虫模块, 使数据更规范， stripe()函数
+
+排序函数 __sort(): 
+排序的时候必须制定排序类型 key , sorted(..., key = ..., reverse = True)
+reverse
+如果需要，把字符串转换成数字
+
+展示函数 __show()
 
 """ 
 
@@ -59,14 +66,13 @@ class Spider(): # 爬取数据
             name = re.findall(Spider.name_pattern, html)
             hot = re.findall(Spider.hot_pattern, html)
             anchor = {'name':name, 'hot':hot}  # 字典形式存储需要的数据类型
-            anchors.append(anchor)  # 找到的列表信息添加到字典中
+            anchors.append(anchor)  # 找到的字典信息添加到列表中
             sum += 1
 #        a = 1  # 调试变量  
-        # print(anchors) 输出字典完整内容
+        # print(anchors) 输出列表完整内容
         print(anchors[0])
         print('当前总数：', sum)
-        print('Anchors Finished Time: ', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        return anchors
+        return anchors # 这个返回数据为列表
 
     def __refine(self, anchors): # 精炼得到的字典数据类型
         l = lambda anchor:{
@@ -74,6 +80,17 @@ class Spider(): # 爬取数据
             'hot':anchor['hot'][0]
             }
         return map(l, anchors)
+
+    def __sort(self, anchors):  # 排序函数
+        anchors = sorted(anchors, key = self.__sort_seed, reverse = True)  # 对获得的数据排序
+        return anchors  # 
+    
+    def __sort_seed(self, anchor): # 
+        return anchor['hot']
+
+    def __show(self, anchors): #展示函数
+        for anchor in anchors:
+            print(anchor['name'] + '-->' + anchor['hot']) # 打印排序结果
 
     # 入口函数 
     def go(self):  # 公开方法，这里是入口函数
@@ -83,7 +100,10 @@ class Spider(): # 爬取数据
         htmls = self.__fetch_content()
         anchors = self.__analysis(htmls) # 接受私有函数return值
         anchors = list(self.__refine(anchors))
-        print(anchors)
+        anchors = self.__sort(anchors)
+        self.__show(anchors)  # 调用所需函数
+        # print(anchors)        
+        print('Anchors Finished Time: ', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     #    # 定时器，定时半小时执行爬取程序，计算整个过程一次爬取时间，30S-爬取所用时间为间隔时长
     #     crawl_space_time = 30 - (float(int(datetime.datetime.now().timestamp())) - time.mktime(time.strptime(crawl_start_time,'%Y-%m-%d %H:%M:%S')))
     #     # print("==========================================================================================")
@@ -91,6 +111,7 @@ class Spider(): # 爬取数据
     #     # print("==========================================================================================")
     #     t = Timer(crawl_space_time, Spider)
     #     t.start()
+
 
 
 spider = Spider()
