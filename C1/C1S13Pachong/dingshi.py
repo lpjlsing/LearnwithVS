5e781c290fdbfdbde96d0cda5e3389eb06b4473a
lpjlsing <lpjlsing@163.com>
2019-06-14 22:02:15 +0800

    pachong_dingshi_061419

--- C1/C1S13Pachong/dingshi.py
+++ C1/C1S13Pachong/dingshi.py
@@ -9,3 +9,20 @@ def printTime(inc):
 printTime(5)
 
 
+# APSchedular 定时任务框架 BlockingScheduler 和 BackgroundScheduler
+# APScheduler 四个组件分别为：
+# 触发器(trigger)： date, interval, cron(特定周期性)
+# 业存储(job store)：APScheduler 默认使用 MemoryJobStore，可以修改使用 DB 存储方案
+# 执行器(executor)：在作业中提交制定的可调用对象到一个线程或者进城池来进行。
+#                  当作业完成时，执行器将会通知调度器。
+#                  ProcessPoolExecutor    ThreadPoolExecutor*
+# 调度器(scheduler)。
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+# 输出时间
+def job():
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+# BlockingScheduler
+scheduler = BlockingScheduler()
+scheduler.add_job(job, 'cron', day_of_week='1-5', hour=6, minute=30)
+scheduler.start()
--- C1/C1S13Pachong/pachong2.py
+++ C1/C1S13Pachong/pachong2.py
@@ -4,7 +4,7 @@
 
 爬虫
 
-加入定时器
+加入定时器  timer()  APSchedular
 
 精炼爬虫模块, 使数据更规范， stripe()函数
 
@@ -24,6 +24,8 @@ import time
 import re
 from urllib import request
 from threading import Timer # 定时器
+from apscheduler.schedulers.blocking import BlockingScheduler
+# from datetime import datetime
 
 # import requests
  
@@ -126,14 +128,20 @@ class Spider(): # 爬取数据
         self.__show(anchors)  # 调用所需函数
 
         # print(anchors)        
-        print('Anchors Finished Time: ', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-       # 定时器，定时半小时执行爬取程序，计算整个过程一次爬取时间，30S-爬取所用时间为间隔时长
-        crawl_space_time = 30 - (float(int(datetime.datetime.now().timestamp())) - time.mktime(time.strptime(crawl_start_time,'%Y-%m-%d %H:%M:%S')))
-        # print("==========================================================================================")
-        print("       爬取结束!等待下一次爬取,下一次爬取将于[" + str(crawl_space_time) + '] 秒后进行……      ')
-        # print("==========================================================================================")
-        t = Timer(crawl_space_time, Spider)
-        t.start()
+    #     print('Anchors Finished Time: ', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    #    # 定时器，定时半小时执行爬取程序，计算整个过程一次爬取时间，30S-爬取所用时间为间隔时长
+    #     crawl_space_time = 30 - (float(int(datetime.datetime.now().timestamp())) - time.mktime(time.strptime(crawl_start_time,'%Y-%m-%d %H:%M:%S')))
+    #     # print("==========================================================================================")
+    #     print("       爬取结束!等待下一次爬取,下一次爬取将于[" + str(crawl_space_time) + '] 秒后进行……      ')
+    #     # print("==========================================================================================")
+    #     t = Timer(crawl_space_time, Spider)
+    #     t.start()
+
+        # APSchedular 定时
+        # BlockingScheduler
+        sched = BlockingScheduler()
+        sched.add_job(Spider, 'cron', day_of_week=0-6, hour=0, minute=0, second=30, end_date='20190615')
+        sched.start()
 
 
 
