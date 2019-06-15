7386b2e005e024ac0090a6798b7469c3cc8ac815
lpjlsing <lpjlsing@163.com>
2019-06-16 00:15:57 +0800

    pythonic_061619

--- C1/C1S13Pachong/pythonic1.py
+++ C1/C1S13Pachong/pythonic1.py
@@ -9,5 +9,17 @@ b3 = {i*i+ i**3 for i in a if i>=3} # 字典化，元组仍可以推导，输出
 print(b)
 print(b2)
 
+# 对字典表达式推导，需注意key,value是可能需要两个参数
+# 元组要注意元组的不可变性
 
+
+# None: 本身是个对象，NoneType
+# None 为tpye类型，表示不存在；False为bool类型，表示真假
+c1 = ''
+c2 = []
+c3 = False
+c4 = None
+
+if not a: # not判断a是True or False
+if a is None: # is做判断，a 是否和None是一样
 
