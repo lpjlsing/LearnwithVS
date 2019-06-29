f64d0fd972e05551cd85c3e37ef1b717a6b042d7
lpjlsing <lpjlsing@163.com>
2019-06-30 00:59:38 +0800

    pandas

--- C2/Numpys/pandas1.py
+++ C2/Numpys/pandas1.py
@@ -1,3 +1,8 @@
+ """  
+ pandas
+
+ """
+
 from pandas import pandas as pd
 from numpy import numpy as np
 
@@ -7,7 +12,8 @@ a1 = a.head(3) # 默认read前5条数据，
 a2 = a.tail(4) # 后四行
 a3 = a.columns # 每个列的列名
 a4 = a.shape 
-a5 = a.loc[0] # 取出第一行数据(位置为[0])
+
+a5 = a.loc[0,'ww'] # 取出第一行数据(位置为[0]),元素为'ww'的列
 a6 = a.loc[3：6] # 取出第3-6行数据
 a7 = a["aaa"] # aaa为列名，通过列名来定位对应的列，aaa可以使列表    ******
 
@@ -34,6 +40,8 @@ sum(a['aas'])/len(a['aas'])， a['qqq'].mean() # 求平均值
 # pivot_table
 d1 = a(index='a1', value=['a2','a3'], aggfunc=np.mean or np.sum)  # a1为统计的基准， 和a1比较值为a2的关系np.mean， aggfunc默认为求均值 ******
 
+# dropna
+e1 = a.dropna(axis=0, subset['as1','as2']) # 丢掉对应值，dropna。 axis表示维度，找subset列中的值，然后丢掉
 
 
 
