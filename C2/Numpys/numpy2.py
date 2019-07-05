65652206a1c01e5cef8ea894ea085cb4566371c8
lpjlsing <lpjlsing@163.com>
2019-07-06 00:39:41 +0800

    numpy2

--- C2/Numpys/numpy2.py
+++ C2/Numpys/numpy2.py
@@ -4,5 +4,11 @@ numpy: 进阶
 '''
 import numpy as np 
 
-# ndarray.dtype 元素类型
-# ndarray.shape 数组结构   ndim size itemsize flags数组内存信息 real imag data数组元素实际存储区
+
+ndarray.dtype 元素类型  # dtype 默认float
+ndarray.shape 数组结构   ndim size itemsize flags数组内存信息 real imag data数组元素实际存储区
+ndarray.astype()修改元素类型
+ndarray.reshape()重新定义数组结构
+
+np.array() np.zeros  np.empty  np.ones  
+np.eye 对角矩阵
